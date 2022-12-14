104dff42a8cda0f00ca2b269384c41234dc5d1bd
Giovanni Iachello <60585229+giachello@users.noreply.github.com>
2022-12-14 09:30:19 -0800

    Add source reporting

--- test_pybeoplay_async.py
+++ test_pybeoplay_async.py
@@ -7,18 +7,20 @@ LOG = logging.getLogger(__name__)
 
 
 async def main(host):
-    async with aiohttp.ClientSession() as session:
+    timeout = aiohttp.ClientTimeout(total=None, connect=None, sock_connect=None, sock_read=None)
+    async with aiohttp.ClientSession(timeout = timeout) as session:
         gateway = BeoPlay(host, session)
-        
+
         await gateway.async_getDeviceInfo()
         print ("Serial Number: " , gateway._serialNumber)
         print ("Type Number: ", gateway._typeNumber)
         print ("Item Number: ",gateway._itemNumber)
         print ("Name: ",gateway._name)
         print ("Standby: ", gateway.on)
-        
+
         def callback():
             print ("On State: " , gateway.on)
+            print ("Source: ", gateway.source)
             print ("Min Volume: " , gateway.min_volume)
             print ("Max Volume: " , gateway.max_volume)
             print ("Volume: " , gateway.volume)
@@ -28,7 +30,10 @@ async def main(host):
             print ("Media track: " , gateway.media_track)
             print ("Media artist: " , gateway.media_artist)
             print ("Media album: " , gateway.media_album)
-            print ("Primary experience: " , gateway.primary_experience)            
+            print ("Media genre: " , gateway.media_genre)
+            print ("Media country: " , gateway.media_country)
+            print ("Media languages: " , gateway.media_languages)
+            print ("Primary experience: " , gateway.primary_experience)
 
         r = await  gateway.async_notificationsTask(callback)
         print (str(r))
@@ -47,5 +52,3 @@ if __name__ == '__main__':
         quit()
 
     asyncio.run(main(sys.argv[1]))
-
-
